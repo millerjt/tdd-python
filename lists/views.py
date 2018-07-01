7d2f1ab4c75e4b168f85ca7f36790551b74e7f2a
James Miller <jamesm9@gmail.com>
2018-07-01 12:31:31 -0400

    Submit and show items using POST

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
